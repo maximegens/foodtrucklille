72c4f3f0c0915ea87d8774e970a2bd6a4cf08cc0
Maxime Gens <maximegens@gmail.com>
2016-02-14 17:23:03 +0100

    Remplacement de l'actionBar par une ToolBar

--- app/src/main/java/com/maximegens/foodtrucklillois/MainActivity.java
+++ app/src/main/java/com/maximegens/foodtrucklillois/MainActivity.java
@@ -3,6 +3,7 @@ package com.maximegens.foodtrucklillois;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.maximegens.foodtrucklillois.fragments.ListeFoodTruckFragment;
@@ -16,6 +17,15 @@ public class MainActivity extends AppCompatActivity implements ListeFoodTruckFra
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //definir notre toolbar en tant qu'actionBar
+        setSupportActionBar(toolbar);
+
+        //afficher le bouton retour
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Commit du Fragment des listes des FT
         getSupportFragmentManager().beginTransaction()
