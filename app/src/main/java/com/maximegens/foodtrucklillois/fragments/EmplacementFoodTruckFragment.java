b07bca7b8c8cb9e54b9a97cbe018c214c28ad95d
Maxime Gens <maxime.gens@atos.net>
2016-04-14 12:31:03 +0200

    Modification fab favori

--- app/src/main/java/com/maximegens/foodtrucklillois/fragments/EmplacementFoodTruckFragment.java
+++ app/src/main/java/com/maximegens/foodtrucklillois/fragments/EmplacementFoodTruckFragment.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
