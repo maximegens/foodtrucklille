2476d050b7db71d6b640cf665cc892c7d5219782
Maxime Gens <maxime.gens@atos.net>
2016-04-27 16:59:24 +0200

    Changement couleur loader

--- app/src/main/java/com/maximegens/foodtrucklillois/fragments/ListeFoodTruckFragment.java
+++ app/src/main/java/com/maximegens/foodtrucklillois/fragments/ListeFoodTruckFragment.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -26,6 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.maximegens.foodtrucklillois.R;
