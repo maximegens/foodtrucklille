8f736b88e98c6683160f7c7f6560c4cc7d89b4ae
Maxime Gens <maximegens.developpement@gmail.com>
2017-05-10 09:33:21 +0000

    Passage vers google drive

--- app/src/main/java/com/maximegens/foodtrucklillois/network/RetreiveListeFTService.java
+++ app/src/main/java/com/maximegens/foodtrucklillois/network/RetreiveListeFTService.java
@@ -5,7 +5,7 @@ import com.maximegens.foodtrucklillois.beans.FoodTruckApp;
 import retrofit.http.GET;
 
 /**
- * Interface Service pour la recuperation de donnÃ©es via RetroFit.
+ * Interface Service pour la recuperation de données via RetroFit.
  */
 interface RetreiveListeFTService {
 
@@ -13,6 +13,6 @@ interface RetreiveListeFTService {
      * Recuperation de la liste des FoodTrucks.
      * @return l'objet contenant toutes les informations sur les foodTrucks.
      */
-    @GET("/u/61408511/FoodTrucksApp.json")
+    @GET("/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/6ljoo2h0plenc6hd764f4nttival0ihu/1494403200000/00153049752493752133/*/0B0WKDOwC2VM-Mkh3UXVQZXFxYVk?e=download")
     FoodTruckApp getListFT();
 }
